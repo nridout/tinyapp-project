73cddb887a86d517c6f3257b8cf1567691e934c5
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-12 12:29:47 -0500

    Added Express Web Server

---
+++ express_server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var PORT = 3000; // default port 3000
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
