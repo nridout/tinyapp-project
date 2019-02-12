07de90cc8d053dff8f9bebf8a57350e2dc0ac851
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-12 13:12:16 -0500

    Installed EJS and set it up in the server file

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
-var PORT = 3000; // default port 3000
+var PORT = 3000; // default port 3000 because 8080 isn't working for me
+
+app.set("view engine", "ejs")
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
