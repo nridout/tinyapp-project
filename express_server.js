10e4fd58d3dcc642b6e208ee3d859b9b8598f14d
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-12 14:58:57 -0500

    Added route to display single URL

--- express_server.js
+++ express_server.js
@@ -17,8 +17,14 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
