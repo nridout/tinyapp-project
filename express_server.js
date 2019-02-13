42827aa7282f468ceec7947a9c1a0d744725ae14
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-13 11:46:08 -0500

    reorganized and commented the code structure

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 // import express, the web application framework
-var express = require("express");
-var app = express();
-var PORT = 3000; // default port 3000 because 8080 isn't working for me
+const express = require("express")
+const bodyParser = require("body-parser");
+const app = express()
+const PORT = 3000 // default port 3000 because 8080 isn't working for me
 
 // sets the view engine to ejs, a templating language that generates HTML markup with plain JavaScript
 app.set("view engine", "ejs")
@@ -11,25 +12,23 @@ var urlDatabase = {
   '4cef9b': 'http://www.lighthouselabs.ca',
   '5ce85d': 'http://www.test.ca',
   '1a4aa5': 'http://www.google.ca',
-};
-// ******is this still necessary????*****
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+}
+
+// MIDDLEWARE
 
 // formats the response from the original url submission
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// console message indicating which port the server is running on
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+// ROUTES
+
+// ---ROOT HOMEPAGE
 
 // sets the template for the root (homepage)
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls/new");
 });
+
+// ---URL LIST
 
 // sets the template for the list of all long & short urls
 // exports the database info to the template
@@ -50,17 +49,19 @@ app.post("/urls", (req, res) => {
   res.redirect(redirectRandom);
 });
 
-// redirects the long URL to the short URL
-// when the unique key is added to the /u/ path
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
+// function that generates a random string for the short url
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
+
+// ---NEW URL
 
 // sets the template for the short URL generation page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+// ---URL ID PAGE
 
 // sets the template for the unique id short URL page
 app.get("/urls/:shortURL", (req, res) => {
@@ -71,20 +72,30 @@ app.get("/urls/:shortURL", (req, res) => {
 // deletes short urls
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete(urlDatabase[req.params.shortURL]);
-  let randomString = generateRandomString();
-  let longURL = req.body.longURL;
-  urlDatabase[randomString] = longURL;
-  var redirectRandom = "/urls/" + randomString;
-  res.redirect(redirectRandom);
-});
-
-// edits short urls
-app.post("/urls/:shortURL/edit", (req, res) => {
-  delete (urlDatabase[req.params.shortURL]);
   res.redirect("/urls");
 });
 
-// function that generates a random string for the short url
-function generateRandomString() {
-  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
-}
+// reassigns shorty to a new url
+app.post("/urls/:shortURL/edit", (req, res) => {
+  // let longURL = req.body.longURL;
+  // let shortURL =
+  // urlDatabase[??] = longURL;
+  // var redirectShort = "/urls/" + ??;
+  res.redirect(redirectShort);
+});
+
+// ---SHORT URL REDIRECT
+
+// redirects the long URL to the short URL
+// when the unique key is added to the /u/ path
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+// PORT LISTENER
+
+// console message indicating which port the server is running on
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
