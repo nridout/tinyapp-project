2535c80c28443596762cadc58dbdf76493c193e8
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-15 19:12:23 -0500

    refactoring code

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-// Required modules
+// MODULES
 const express = require("express")
 const bodyParser = require("body-parser")
 var cookieSession = require('cookie-session')
@@ -7,10 +7,14 @@ const bcrypt = require('bcrypt');
 const PORT = 3000 // default port 3000 because 8080 isn't working for me
 
 
-// Sets the view engine to ejs
+// EJS VIEW ENGINE
 app.set("view engine", "ejs")
 
-// MIDDLEWARE
+// ---------------------------------------------------------------------------------- //
+
+//                                    MIDDLEWARE
+
+// ---------------------------------------------------------------------------------- //
 
 // formats the response from the original url submission
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -21,7 +25,11 @@ app.use(cookieSession({
   keys: ["13534626"],
 }))
 
-// ROUTES
+// ---------------------------------------------------------------------------------- //
+
+//                                      ROUTES
+
+// ---------------------------------------------------------------------------------- //
 
 // ** DATABASES **
 
@@ -32,42 +40,49 @@ const urlDatabase = {}
 const users = {}
 
 
-// ---ROOT HOMEPAGE
+// HOMEPAGE
+// ---------------------------------------------------------------------------------- //
 
-// sets the template for the root (homepage)
+// Sets the template for the Homepage
 app.get("/", (req, res) => {
-
   let templateVars = {
     userInfo: users[req.session.user_id]
   }
-
   res.render("urls_home", templateVars)
 })
 
-// --- REGISTER
 
-// template for the register page
+// REGISTER
+// ---------------------------------------------------------------------------------- //
+
+// -- GET -- //
+// Sets the template for the Register Page
 app.get("/register", (req, res) => {
-
   let templateVars = {
     userInfo: users[req.session.user_id]
   }
-
   res.render("urls_register", templateVars)
 })
 
+// -- POST -- //
 // Handles Registration Submission
 app.post("/register", (req, res) => {
-  const id = generateRandomString() // Generates a random user id
+  // Generates a random user id
+  const id = generateRandomString()
+  // Req email & password from Registration Form
   const email = req.body.email
   const password = req.body.password
+  // Hashes password
   const hashedPassword = bcrypt.hashSync(password, 10);
+
   // Checks that the user has submitted an email & password
   if (email === "" || password === "" ) {
-    return res.status(400).json({ message: 'Contact name is required' })
+    return res.status(400).send('Please enter and email and password')
+
   // Checks that the user email is not already in the database
   } else if (found(email)) {
-    return res.status(404).json({ message: 'This email already exists' })
+    return res.status(404).send('This email already exists')
+
   // If both conditions met, creates new user in the user database
   } else {
     users[id] = {
@@ -92,7 +107,9 @@ const found = inputEmail => {
 }
 
 // --- LOGIN
+// ---------------------------------------------------------------------------------- //
 
+// -- GET -- //
 // template for the login page
 app.get("/login", (req, res) => {
   let templateVars = {
@@ -101,39 +118,42 @@ app.get("/login", (req, res) => {
   res.render("urls_login", templateVars)
 })
 
+// -- POST -- //
 // Handles the Login Submission
 app.post("/login", (req, res) => {
   const email = req.body.email
   const checkEmail = found(req.body.email)
   const password = req.body.password
   let user_id = correctPassword(password, email)
+
   // Checks that the email does exist in the user db
   if (!checkEmail) {
-    return res.status(403).json({ message: 'User not found' })
+    return res.status(403).send( 'User not found' )
+
   // Checks that the email & password match
   } else if (!user_id) {
-    return res.status(403).json({ message: 'Password incorrect' })
+    return res.status(403).send( 'Password incorrect' )
+
   // sets the cookie to the user_id & redirects to urls page
   } else {
     req.session.user_id = user_id
     res.redirect("/urls")
   }
-})
-
-// Checks that the UserID & Password Match
-// If they match, returns the user id
-const correctPassword = (inputPassword, inputEmail)  => {
-  let passwordsMatch = bcrypt.compareSync(inputPassword, users[id].password)
-  for (id in users) {
-    if (users[id].email === inputEmail && passwordsMatch) {
-      return users[id].id
+  // Checks that the UserID & Password Match
+  // If they match, returns the user id
+  const correctPassword = (inputPassword, inputEmail) => {
+    let passwordsMatch = bcrypt.compareSync(inputPassword, users[id].password)
+    for (id in users) {
+      if (users[id].email === inputEmail && passwordsMatch) {
+        return users[id].id
+      }
     }
   }
-}
+})
 
-// --- LOGOUT
 
-// deletes user cookie when user logs out
+
+// handles logout - deletes session cookie
 app.post("/logout", (req, res) => {
 
   let user_id = req.body.user_id
@@ -141,8 +161,10 @@ app.post("/logout", (req, res) => {
   res.redirect("/login")
 })
 
-// ---URL INDEX
+// ---USER URL INDEX
+// ---------------------------------------------------------------------------------- //
 
+// -- GET -- //
 // sets the template for the list of all long & short urls
 // exports the database info to the template
 app.get("/urls", (req, res) => {
@@ -187,7 +209,7 @@ const userOwnsURL = function (userShortURL, user_id) {
   }
 }
 
-
+// -- POST -- //
 // collects the input from the "long URL" form
 // assigns a random string to the short URL
 // generates a unique id page for the short URL
@@ -209,8 +231,10 @@ function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1)
 }
 
-// ---NEW URL
+// ---CREATE NEW URL
+// ---------------------------------------------------------------------------------- //
 
+// -- GET -- //
 // sets the template for the short URL generation page
 app.get("/urls/new", (req, res) => {
   // Check if the user is registered & logged in
@@ -226,8 +250,10 @@ app.get("/urls/new", (req, res) => {
 
 })
 
-// ---URL ID PAGE
+// ---CUSTOM URL ID PAGE
+// ---------------------------------------------------------------------------------- //
 
+// -- GET -- //
 // sets the template for the unique id short URL page
 app.get("/urls/:shortURL", (req, res) => {
   shortURL = req.params.shortURL
@@ -246,6 +272,7 @@ app.get("/urls/:shortURL", (req, res) => {
   }
 })
 
+// -- POST -- //
 // deletes short urls
 app.post("/urls/:shortURL/delete", (req, res) => {
   let shortURL = req.params.shortURL
@@ -258,8 +285,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   }
 })
 
-
-
+// -- POST -- //
 // reassigns shorty to a new url
 app.post("/urls/:id", (req, res) => {
   let user_id = req.session.user_id
@@ -273,7 +299,9 @@ app.post("/urls/:id", (req, res) => {
 });
 
 // ---SHORT URL REDIRECT
+// ---------------------------------------------------------------------------------- //
 
+// -- GET -- //
 // redirects the long URL to the short URL
 // when the unique key is added to the /u/ path
 app.get("/u/:shortURL", (req, res) => {
@@ -282,6 +310,7 @@ app.get("/u/:shortURL", (req, res) => {
 })
 
 // PORT LISTENER
+// ---------------------------------------------------------------------------------- //
 
 // console message indicating which port the server is running on
 app.listen(PORT, () => {
