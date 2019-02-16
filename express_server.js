f46eb886f71a54426dd5dccf96f25b24b8c8619f
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-15 21:01:46 -0500

    Refactoring code

--- express_server.js
+++ express_server.js
@@ -1,14 +1,14 @@
 // MODULES
-const express = require("express")
-const bodyParser = require("body-parser")
-var cookieSession = require('cookie-session')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+var cookieSession = require('cookie-session');
+const app = express();
 const bcrypt = require('bcrypt');
 const PORT = 3000 // default port 3000 because 8080 isn't working for me
 
 
 // EJS VIEW ENGINE
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 // ---------------------------------------------------------------------------------- //
 
@@ -17,13 +17,13 @@ app.set("view engine", "ejs")
 // ---------------------------------------------------------------------------------- //
 
 // formats the response from the original url submission
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Parse Cookie header and populate req.cookies with an object keyed by the cookie names
 app.use(cookieSession({
   name: 'session',
-  keys: ["13534626"],
-}))
+  keys: ["1353462werwr2323r6"],
+}));
 
 // ---------------------------------------------------------------------------------- //
 
@@ -34,13 +34,13 @@ app.use(cookieSession({
 // ** DATABASES **
 
 // URL Database
-const urlDatabase = {}
+const urlDatabase = {};
 
 // User Database
-const users = {}
+const users = {};
 
 
-// HOMEPAGE
+// HOMEPAGE ---- /
 // ---------------------------------------------------------------------------------- //
 
 // Sets the template for the Homepage
@@ -48,146 +48,38 @@ app.get("/", (req, res) => {
   let templateVars = {
     userInfo: users[req.session.user_id]
   }
-  res.render("urls_home", templateVars)
-})
+  res.render("urls_home", templateVars);
+});
 
 
-// REGISTER
+
+// USER URL INDEX ---- /urls
 // ---------------------------------------------------------------------------------- //
 
-// -- GET -- //
-// Sets the template for the Register Page
-app.get("/register", (req, res) => {
-  let templateVars = {
-    userInfo: users[req.session.user_id]
-  }
-  res.render("urls_register", templateVars)
-})
+// -- GET /urls -- //
 
-// -- POST -- //
-// Handles Registration Submission
-app.post("/register", (req, res) => {
-  // Generates a random user id
-  const id = generateRandomString()
-  // Req email & password from Registration Form
-  const email = req.body.email
-  const password = req.body.password
-  // Hashes password
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-  // Checks that the user has submitted an email & password
-  if (email === "" || password === "" ) {
-    return res.status(400).send('Please enter and email and password')
-
-  // Checks that the user email is not already in the database
-  } else if (found(email)) {
-    return res.status(404).send('This email already exists')
-
-  // If both conditions met, creates new user in the user database
-  } else {
-    users[id] = {
-      id: id,
-      email: email,
-      password: hashedPassword
-    }
-  }
-  // Sets the user id cookie to the user id
-  req.session.user_id = id
-  // Redirects user to the urls page
-  res.redirect("/urls")
-})
-
-// Checks if email is already in the user database
-const found = inputEmail => {
-  for (id in users) {
-    if (users[id].email === inputEmail) {
-      return true
-    }
-  }
-}
-
-// --- LOGIN
-// ---------------------------------------------------------------------------------- //
-
-// -- GET -- //
-// template for the login page
-app.get("/login", (req, res) => {
-  let templateVars = {
-    userInfo: users[req.session.user_id]
-  }
-  res.render("urls_login", templateVars)
-})
-
-// -- POST -- //
-// Handles the Login Submission
-app.post("/login", (req, res) => {
-  const email = req.body.email
-  const checkEmail = found(req.body.email)
-  const password = req.body.password
-  let user_id = correctPassword(password, email)
-
-  // Checks that the email does exist in the user db
-  if (!checkEmail) {
-    return res.status(403).send( 'User not found' )
-
-  // Checks that the email & password match
-  } else if (!user_id) {
-    return res.status(403).send( 'Password incorrect' )
-
-  // sets the cookie to the user_id & redirects to urls page
-  } else {
-    req.session.user_id = user_id
-    res.redirect("/urls")
-  }
-  // Checks that the UserID & Password Match
-  // If they match, returns the user id
-  const correctPassword = (inputPassword, inputEmail) => {
-    let passwordsMatch = bcrypt.compareSync(inputPassword, users[id].password)
-    for (id in users) {
-      if (users[id].email === inputEmail && passwordsMatch) {
-        return users[id].id
-      }
-    }
-  }
-})
-
-
-
-// handles logout - deletes session cookie
-app.post("/logout", (req, res) => {
-
-  let user_id = req.body.user_id
-  req.session = null
-  res.redirect("/login")
-})
-
-// ---USER URL INDEX
-// ---------------------------------------------------------------------------------- //
-
-// -- GET -- //
-// sets the template for the list of all long & short urls
-// exports the database info to the template
+// Sets the template for the custom urls
 app.get("/urls", (req, res) => {
-  console.log(req.session.user_id)
   // Check if the user is registered & logged in
   if (!req.session.user_id) {
-    // redirect unregistered users to login page
-    res.redirect("/login")
+    // Redirect users not logged in to the Login page
+    res.redirect("/login");
   } else {
-    let userInfo = users[req.session.user_id]
-    // let user_id = req.session.user_id
-    let userURLdatabase = urlsForUser(req.session.user_id) //userURLdatabase
+    let userInfo = users[req.session.user_id];
+    // Sets the user database with the user's custom URLs
+    let userURLdatabase = urlsForUser(req.session.user_id);
     let templateVars = {
       userInfo: userInfo,
       urls: userURLdatabase
-    }
-
-    console.log(userURLdatabase)
-    res.render("urls_index", templateVars)
+    };
+    res.render("urls_index", templateVars);
   }
 })
 
-// filters the urlDatabase by user id
+// -- FUNCTIONS -- //
+
+// Function that filters the urlDatabase by user id
+// And returns an object with the user's custom URLs
 const urlsForUser = user_id => {
   const userURLs = {}
   for (shortURL in urlDatabase) {
@@ -198,23 +90,22 @@ const urlsForUser = user_id => {
   return userURLs
 }
 
-//checks if shortURL is in user DB
-// filters the urlDatabase by user id
-const userOwnsURL = function (userShortURL, user_id) {
+// Function that checks if the user owns a given short URL
+const userOwnsURL = function (givenShortURL, user_id) {
   let userURLdatabase = urlsForUser(user_id)
   for (shortURL in userURLdatabase) {
-    if (shortURL === userShortURL) {
+    if (shortURL === givenShortURL) {
       return true
     }
   }
 }
 
-// -- POST -- //
-// collects the input from the "long URL" form
-// assigns a random string to the short URL
-// generates a unique id page for the short URL
-// redirects the user to the unique id page upon submission
+// -- POST /urls -- //
+
+// Generates a custom short URL for a given long URL &
+// Redirects the user to the custom URL id page upon submission
 app.post("/urls", (req, res) => {
+  // Assigns a random string to the short URL
   let randomString = generateRandomString()
   let longURL = req.body.longURL
   let user_id = req.session.user_id
@@ -231,15 +122,16 @@ function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1)
 }
 
-// ---CREATE NEW URL
+// CREATE NEW URL ---- /urls/new
 // ---------------------------------------------------------------------------------- //
 
-// -- GET -- //
+// -- GET /urls/new -- //
+
 // sets the template for the short URL generation page
 app.get("/urls/new", (req, res) => {
-  // Check if the user is registered & logged in
+  // Check if the user is logged in
   if (!req.session.user_id) {
-    // redirect unregistered users to login page
+    // Redirect users not logged in to the Login page
     res.redirect("/login")
   } else {
     let templateVars = {
@@ -247,20 +139,22 @@ app.get("/urls/new", (req, res) => {
     }
     res.render("urls_new", templateVars)
   }
-
 })
 
-// ---CUSTOM URL ID PAGE
+// ---CUSTOM URL ID PAGE ---- /urls/:id
 // ---------------------------------------------------------------------------------- //
 
-// -- GET -- //
+// -- GET /urls/:id -- //
+
 // sets the template for the unique id short URL page
 app.get("/urls/:shortURL", (req, res) => {
   shortURL = req.params.shortURL
   // Check if the user is registered & logged in & has url in userdb
   if (!req.session.user_id || !userOwnsURL(shortURL, req.session.user_id)) {
     // redirect unregistered users to login page
-    return res.status(401).json({ message: 'Request Denied, please log in to delete your link' })
+    return res
+    .status(401)
+    .send( 'Request Denied, please log in' )
   } else {
     let templateVars = {
       userInfo: users[req.session.user_id],
@@ -268,7 +162,6 @@ app.get("/urls/:shortURL", (req, res) => {
       longURL: urlDatabase[req.params.shortURL].longURL,
     }
     res.render("urls_show", templateVars)
-    console.log(longURL)
   }
 })
 
@@ -278,7 +171,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   let shortURL = req.params.shortURL
   // Check if the user is registered & logged in & has url in userdb
   if (!req.session.user_id && !userOwnsURL(shortURL)) {
-    return res.status(401).json({ message: 'Request Denied, please log in to delete your link' })
+    return res.status(401).json({ message: 'Request Denied, please log in' })
   } else {
     delete (urlDatabase[shortURL])
   res.redirect("/urls")
@@ -308,6 +201,119 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]["longURL"];
   res.redirect(longURL)
 })
+
+// --- LOGIN
+// ---------------------------------------------------------------------------------- //
+
+// -- GET -- //
+// template for the login page
+app.get("/login", (req, res) => {
+  let templateVars = {
+    userInfo: users[req.session.user_id]
+  }
+  res.render("urls_login", templateVars)
+})
+
+// -- POST -- //
+// Handles the Login Submission
+app.post("/login", (req, res) => {
+  const email = req.body.email
+  const checkEmail = found(req.body.email)
+  const password = req.body.password
+  let user_id = correctPassword(password, email)
+
+  // Checks that the email does exist in the user db
+  if (!checkEmail) {
+    return res.status(403).send('User not found')
+
+    // Checks that the email & password match
+  } else if (!user_id) {
+    return res.status(403).send('Password incorrect')
+
+    // sets the cookie to the user_id & redirects to urls page
+  } else {
+    req.session.user_id = user_id
+    res.redirect("/urls")
+  }
+  // Checks that the UserID & Password Match
+  // If they match, returns the user id
+  const correctPassword = (inputPassword, inputEmail) => {
+    let passwordsMatch = bcrypt.compareSync(inputPassword, users[id].password)
+    for (id in users) {
+      if (users[id].email === inputEmail && passwordsMatch) {
+        return users[id].id
+      }
+    }
+  }
+})
+
+
+
+// REGISTER ----
+// ---------------------------------------------------------------------------------- //
+
+// -- GET -- //
+// Sets the template for the Register Page
+app.get("/register", (req, res) => {
+  let templateVars = {
+    userInfo: users[req.session.user_id]
+  }
+  res.render("urls_register", templateVars)
+})
+
+// -- POST -- //
+// Handles Registration Submission
+app.post("/register", (req, res) => {
+  // Generates a random user id
+  const id = generateRandomString()
+  // Req email & password from Registration Form
+  const email = req.body.email
+  const password = req.body.password
+  // Hashes password
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  // Checks that the user has submitted an email & password
+  if (email === "" || password === "") {
+    return res.status(400).send('Please enter and email and password')
+
+    // Checks that the user email is not already in the database
+  } else if (found(email)) {
+    return res.status(404).send('This email already exists')
+
+    // If both conditions met, creates new user in the user database
+  } else {
+    users[id] = {
+      id: id,
+      email: email,
+      password: hashedPassword
+    }
+  }
+  // Sets the user id cookie to the user id
+  req.session.user_id = id
+  // Redirects user to the urls page
+  res.redirect("/urls")
+})
+
+// Checks if email is already in the user database
+const found = inputEmail => {
+  for (id in users) {
+    if (users[id].email === inputEmail) {
+      return true
+    }
+  }
+}
+
+// LOGOUT ----
+// ---------------------------------------------------------------------------------- //
+
+// handles logout - deletes session cookie
+app.post("/logout", (req, res) => {
+
+  let user_id = req.body.user_id
+  req.session = null
+  res.redirect("/login")
+})
+
 
 // PORT LISTENER
 // ---------------------------------------------------------------------------------- //
